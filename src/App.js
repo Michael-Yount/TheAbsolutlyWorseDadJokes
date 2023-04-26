98bc00b35411f104c2169dc434864b18ee9247c8
Michaeal Yount <cheshirekatsmile101@gmail.com>
2023-04-26 19:05:51 -0400

    fetch dad jokes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect } from 'react';
+import axios from 'axios';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  //data storage
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    
+  const loadData =async () => {
+    const response = await axios.get("https://icanhazdadjoke.com/")
+    console.log(response);
+    setData(response);
+  }
+  loadData();
+}, []);
+
+  return (
+    <div className="container">
+        <h3>The absolutly Worst Dad jokes.</h3>
+        <div className="joke" id="joke"></div>
+        <button className="btn" id="get_joke">Get Another Joke</button>
+      </div>
+  )
+}
 export default App;
