a950b29e8ff12aee41a2d89ca44e559eafc3584e
Michael A. Yount <cheshirekatsmile101@gmail.com>
2023-04-27 18:39:25 -0400

    fixed the button!

--- src/App.js
+++ src/App.js
@@ -6,21 +6,23 @@ import './App.css';
 function App() {
   //data storage
   const [joke, setJoke] = useState([]);
+  const [state, setState] = useState(1);
+
   const API_LINK = "https://icanhazdadjoke.com/";
 
-  useEffect (() => {  
+useEffect (() => {  
   const loadData = async () => {
     const response = await axios.get(`${API_LINK}`, {headers: {Accept:"application/json"}})
-    console.log(response.data.joke);
     setJoke(response.data.joke);
   }
   loadData();
-}, []);
+}, [state]);
+
   return (
     <div className="container">
-        <h2 className='title'>The Absolutely Best Dad jokes.</h2>
+        <h2 className='title'>The Absolute Best Dad jokes.</h2>
         <div className="joke">{joke}</div>
-        <button className="btn btn-primary">Get Another Joke</button>
+        <button className='btn btn-outline-primary' onClick={() => setState(state + 1)}>Get Another Joke</button>
       </div>
   )
 }
